92a4b924956e32273fb8ca99aa76c02ce071c7b1
z1zhang <2659445660@qq.com>
2022-02-13 17:38:40 +0800

    Initial commit

---
+++ src/main/java/com/example/maildemo/MailDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.maildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhang
+ */
+@SpringBootApplication
+public class MailDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maildemo/MailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
